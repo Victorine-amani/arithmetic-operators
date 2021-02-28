17c71dc1743327b8682e7e94fd1fca6f9aa7e35b
Victorine-Amani <victorinenyagwala@gmail.com>
2021-02-28 11:57:43 +0300

    kotlin assignent

---
+++ src/assignment.kt
@@ -0,0 +1,29 @@
+fun main(){
+    name()
+    rem()
+    sum()
+    me()
+}
+fun name() {
+    val name =("Hello Victorine Amani")
+    println(name)
+
+}
+fun rem() {
+    var a = 37
+    var b = 20
+    var modules = a % b
+    println(modules)
+}
+fun sum() {
+    var a = 10
+    var b = 12
+    var c = 32
+    var d = 17
+    var sum = (a+b+c+d)
+    println(sum)
+}
+fun me() {
+    val int = ("I love listening to rhumba")
+    println(int)
+}
